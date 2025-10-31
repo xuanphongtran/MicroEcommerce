d106dc85f61846ed1a96eaf50a2eeea7c3515de5
Xuan Phong Tran <phongbvb.09@gmail.com>
2025-10-31 09:33:15 +0700

    creating catalog seed data

---
+++ Catalog.Infrastructure/Data/TypeContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class TypeContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductType> typeCollection)
+        {
+            bool checkTypes = typeCollection.Find(p => true).Any();
+            string path = Path.Combine("Data", "SeedData", "types.json");
+            if (!checkTypes)
+            {
+                var typesData = File.ReadAllText(path);
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                if (types != null)
+                {
+                    foreach (var item in types)
+                    {
+                        typeCollection.InsertOneAsync(item);
+                    }
+                }
+            }
+        }
+
+    }
+}
