b78f18d248854f2f2ec8efb011e7e96a228a0131
Xuan Phong Tran <phongbvb.09@gmail.com>
2025-10-31 08:56:23 +0700

    create product repositories

---
+++ Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByBrand(string name);
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
