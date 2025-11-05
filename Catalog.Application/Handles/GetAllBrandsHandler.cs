e4abcb2d330689b90bcb4bdebba92d31cce9cc54
Xuan Phong Tran <phongbvb.09@gmail.com>
2025-11-05 13:53:44 +0700

    create brands handlers

---
+++ Catalog.Application/Handles/GetAllBrandsHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handles
+{
+    public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllBrandsHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+        public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var brandList = await _brandRepository.GetAllBrands();
+            var brandResponseList = _mapper.Map<IList<ProductBrand>, IList<BrandResponse>>(brandList.ToList());
+
+            return brandResponseList;
+        }
+    }
+}
---
+++ Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Mappers
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+        }
+    }
+}
