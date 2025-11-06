f26f3c7d326b2b1616703ad3220ac40b8d92c232
Phong Tran <68213989+xuanphongtran@users.noreply.github.com>
2025-11-06 21:33:00 +0700

    Ft/catalog (#3)
    * create product entity
    
    * create product repositories
    
    * creating catalog seed data
    
    * implement catalog context
    
    * implement product repository
    
    * create brands handlers
    
    * create types handler

---
+++ Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Mappers
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+            CreateMap<Product, ProductResponse>().ReverseMap();
+            CreateMap<ProductType, TypeResponse>().ReverseMap();
+        }
+    }
+}
