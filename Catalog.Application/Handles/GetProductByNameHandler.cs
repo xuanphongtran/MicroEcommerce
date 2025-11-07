915943dee2d17c224c78d52e36c0e28aece4cf74
xuanphongtran <phongbvb.09@gmail.com>
2025-11-07 23:07:01 +0700

    implement get product handler

---
+++ Catalog.Application/Handles/GetProductByNameHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handles
+{
+    public class GetProductByNameHandler : IRequestHandler<GetProductByNameQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByNameHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<IList<ProductResponse>> Handle(GetProductByNameQuery request, CancellationToken cancellationToken)
+        {
+            var productList = await  _productRepository.GetProductsByName(request.Name);
+            var productResponseList = ProductMapper.Mapper.Map<IList<ProductResponse>>(productList);
+            return productResponseList; 
+        }
+    }
+}
---
+++ Catalog.Application/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByIdQuery : IRequest<ProductResponse>
+    {
+        public string ID { get; set; }
+        public GetProductByIdQuery(string id)
+        {
+            ID = id;    
+        }
+    } 
+
+}
