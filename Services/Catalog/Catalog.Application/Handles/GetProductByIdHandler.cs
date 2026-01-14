1dece34e6559ce0bc89c6630c5ad498e30991d4e
Phong Tran <68213989+xuanphongtran@users.noreply.github.com>
2026-01-14 09:53:33 +0700

    Ft/catalog (#4)
    * create product entity
    
    * create product repositories
    
    * creating catalog seed data
    
    * implement catalog context
    
    * implement product repository
    
    * create brands handlers
    
    * create types handler
    
    * implement get product handler
    
    * implement create, update, delete product handler
    
    * init base controller
    
    * create catalog controller
    
    * fix folder structure
    
    ---------
    
    Co-authored-by: Trần Xuân Phong <xuanphong@mobcec.com>

---
+++ Services/Catalog/Catalog.Application/Handles/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handles
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await  _productRepository.GetProduct(request.ID);
+            var productResponse = ProductMapper.Mapper.Map<ProductResponse>(product);
+            return productResponse; 
+        }
+    }
+}
