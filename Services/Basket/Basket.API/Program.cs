74d2b2d37bd8b4782347c6d8d94badcf7df9a37e
xuanphongtran <phongbvb.09@gmail.com>
2026-03-25 22:19:26 +0700

    update docker file and docker compose

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,9 @@
+using Basket.Application.Handlers;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Repositories;
+using Basket.Infrastructure.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,30 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add application services
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register mediatr
+var assemblies = new Assembly[]
+{
+    Assembly.GetExecutingAssembly(),
+    typeof (CreateShoppingCartHandler).Assembly
+};
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+
+// Options pattern for Redis configuration
+builder.Services.Configure<CacheSettings>(builder.Configuration.GetSection("CacheSettings"));
+
+// Redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetSection("CacheSettings").GetValue<string>("ConnectionString");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +43,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
