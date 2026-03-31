45ed9dba2e1ff69d7efeb7ec42ebcb063def3a01
xuanphongtran <phongbvb.09@gmail.com>
2026-03-31 08:33:57 +0700

    create discount mapper

---
+++ Services/Discount/Discount.Application/Handlers/GetDiscountHandler.cs
@@ -0,0 +1,38 @@
+﻿using Discount.Application.Dtos;
+using Discount.Application.Mappers;
+using Discount.Application.Queries;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    public class GetDiscountHandler : IRequestHandler<GetDiscountQuery, CouponDto>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public GetDiscountHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+        public async Task<CouponDto> Handle(GetDiscountQuery request, CancellationToken cancellationToken)
+        {
+            // Validation input
+            if (string.IsNullOrWhiteSpace(request.productName))
+            {
+                var validationErrors = new Dictionary<string, string>()
+                {
+                    {"ProductName","Product name must not be empty." }
+                };
+            }
+            // Fetch from repo
+            var coupon = await _discountRepository.GetDiscount(request.productName);
+            if (coupon == null) 
+            {
+                throw new Exception($"Discount for the Product Name = {request.productName} not found.");  
+            }
+            // Mapping
+            return coupon.ToDto();
+
+        }
+    }
+}
---
+++ Services/Discount/Discount.Application/Mappers/CouponMapper.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Application.Dtos;
+using Discount.Core.Entities;
+
+namespace Discount.Application.Mappers
+{
+    public static class CouponMapper
+    {
+        public static CouponDto ToDto(this Coupon coupon)
+        {
+            return new CouponDto(coupon.Id, coupon.ProductName, coupon.Amount, coupon.Description);
+        }
+    }
+}
