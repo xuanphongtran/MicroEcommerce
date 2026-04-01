2511719a0a812e1367303977c260b1af7c6be8dc
xuanphongtran <phongbvb.09@gmail.com>
2026-04-01 10:04:12 +0700

    create database seeder

---
+++ Services/Discount/Discount.Infrastructure/Settings/DbExtension.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Npgsql;
+
+namespace Discount.Infrastructure.Settings
+{
+    public static class DbExtension
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("DbMigration");
+            var databaseSettings = services.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+
+            try
+            {
+                logger.LogInformation("Discount Db Migration Started.");
+                ApplyMigration(databaseSettings.ConnectionString);
+                logger.LogInformation("Discount Db Migration Completed.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An eror occurred while migrating the database.");
+                throw;
+            }
+            return host;
+        }
+
+        private static void ApplyMigration(string connectionString)
+        {
+            var retry = 5;
+            while (retry > 0)
+            {
+                try
+                {
+                    using var connection = new NpgsqlConnection(connectionString);
+                    connection.Open();
+
+                    using var cmd = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+                    cmd.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = @"
+                        CREATE TABLE Coupon(
+                            Id SERIAL PRIMARY KEY,
+                            ProductName VARCHAR(500) NOT NULL,
+                            Description TEXT,
+                            Amount INT
+                        )";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = @"
+                        INSERT INTO Coupon(ProductName, Description, Amount)
+                        VALUES('Adidas Quick Force Indoor Badminton Shoes', 'Shoe Discount', 500)";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = @"
+                        INSERT INTO Coupon(ProductName, Description, Amount)
+                        VALUES('Yonex VCORE Pro 100 A Tennis Racquet (270gm, Strung)', 'Racquet Discount', 700)";
+                    cmd.ExecuteNonQuery();
+                    //success -> exit retry loop
+                    break;
+                }
+                catch
+                {
+                    retry--;
+                    if (retry == 0)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
