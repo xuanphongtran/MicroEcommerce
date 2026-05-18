3dd38fbccf310d396c920acafb29a03345f47598
xuanphongtran <phongbvb.09@gmail.com>
2026-05-18 22:06:05 +0700

    Add OrderContextFactory, seeding, and update dependencies
    Added OrderContextFactory for EF Core design-time support and OrderContextSeed for initial data seeding. Upgraded Microsoft.EntityFrameworkCore.SqlServer and SharpCompress packages, and added Microsoft.Extensions.Configuration.Json for configuration file handling.

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextFactory
+    : IDesignTimeDbContextFactory<OrderContext>
+    {
+        public OrderContext CreateDbContext(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString =
+                configuration.GetConnectionString("OrderingConnectionString");
+
+            var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+
+            optionsBuilder.UseSqlServer(
+                connectionString,
+                sql =>
+                {
+                    // 🔑 THIS IS WHAT EF CLI USES
+                    sql.MigrationsAssembly("Ordering.Infrastructure");
+                });
+
+            return new OrderContext(optionsBuilder.Options);
+        }
+    }
+
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "xuanphong",
+                    FirstName = "Xuan Phong",
+                    LastName = "Tran",
+                    EmailAddress = "xuanphong@gmail.com",
+                    AddressLine = "Ly Thuong Kiet Street",
+                    State = "HCM City",
+                    Country = "Viet Name",
+                    ZipCode = "10000",
+
+                    CardName = "Visa",
+                    CardNumber = "4111111111111111",
+                    CreatedBy = "Xuan Phong",
+                    Expiration = "12/29",
+                    Cvv = "123",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Xuan Phong",
+                    LastModifiedDate = DateTime.UtcNow
+                }
+            };
+        }
+    }
+}
